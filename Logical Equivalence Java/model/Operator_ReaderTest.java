b15ea80a2910ccb2039a2b6a515cb6e6196d5fef
Jacob Canedy <jcanedy19@gmail.com>
2022-03-06 10:02:14 -0500

    Model - Parenthesis Testing

--- Logical Equivalence Java/model/Operator_ReaderTest.java
+++ Logical Equivalence Java/model/Operator_ReaderTest.java
@@ -7,8 +7,10 @@ import org.junit.platform.commons.annotation.Testable;
 public class Operator_ReaderTest {
    @Test
    public void parseTest() {
-      Operator_Reader string1 = new Operator_Reader("p → q v q → p ^ s v r");
-      System.out.println(string1);
+      // Operator_Reader string1 = new Operator_Reader("p → q v q → p ^ s v r");
+      Operator_Reader string2 = new Operator_Reader("(q → p)");
+      Operator_Reader string3 = new Operator_Reader("p → q v (q → p) ^ s v r");
+
    }
     
 
