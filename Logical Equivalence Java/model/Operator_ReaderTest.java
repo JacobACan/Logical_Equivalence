c1b061a69cd83252c112349a657fd382a39328a7
Jacob Canedy <jcanedy19@gmail.com>
2022-03-17 08:58:38 -0400

    Operator_Reader: logic complete

--- Logical Equivalence Java/model/Operator_ReaderTest.java
+++ Logical Equivalence Java/model/Operator_ReaderTest.java
@@ -9,12 +9,12 @@ public class Operator_ReaderTest {
    public void parseTest() {
       Operator_Reader string1 = new Operator_Reader("(p → q v (q → p) ^ s v r)");
       Operator_Reader string2 = new Operator_Reader("(q → p) ^ p");
-      Operator_Reader string8 = new Operator_Reader("(q → p) ^ (p)");
       Operator_Reader string3 = new Operator_Reader("(r ^ (q → s) ^ q ^ p)"); 
       Operator_Reader string4 = new Operator_Reader("(¬r ^ (q → s) ^ q ^ p)");
       Operator_Reader string5 = new Operator_Reader("(¬r ^ (q → s) ^ q ^ p) ^ (¬r ^ (¬q → ¬s) ^ q ^ ¬p)");
       Operator_Reader string6 = new Operator_Reader("(¬r ^ (q → s) ^ q ^ p)) ^ (¬r ^ (¬q → ¬s) ^ q ^ ¬p)");
       Operator_Reader string7 = new Operator_Reader("¬r^(q→s)^q^p)^(¬r^(q→s)^q^p");
+      Operator_Reader string8 = new Operator_Reader("(q → p) ^ (p)");
 
       System.out.println(string5);
    }
