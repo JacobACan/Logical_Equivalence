c7373f99c13faa717822c0ba5df0862629d531b9
Jacob Canedy <jcanedy19@gmail.com>
2022-03-28 14:18:59 -0400

    view : CLI pseudo code

---
+++ Logical Equivalence Java/view/Logical_EquivalenceCLI.java
@@ -0,0 +1,28 @@
+package view;
+
+import model.Logical_Equivalence;
+
+public class Logical_EquivalenceCLI {
+    public static String getEquation() {
+        //TODO : gets input for equation from user and returns it
+        return null;
+    }
+    public static void printEquationsAndEquivalence() {
+        //TODO : prints equations and equivalence
+    }
+    public static boolean testLogicalEquivalence(Logical_Equivalence logical_Equivalence) {
+        /* 
+        getequation1
+        getequation2
+        evalute
+        printEquationsAndEquivalence        
+        */
+        return false;
+    }
+
+
+    public static void main(String[] args) {
+        Logical_Equivalence logical_Equivalence = new Logical_Equivalence();
+        while(testLogicalEquivalence(logical_Equivalence));    
+    }
+}
