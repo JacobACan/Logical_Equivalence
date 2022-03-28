162bd55eba2532207bf2eb85f278b114b23a463d
Jacob Canedy <jcanedy19@gmail.com>
2022-03-28 19:59:51 -0400

    view : GUI start

--- Logical Equivalence Java/view/Logical_EquivalenceGUI.java
+++ Logical Equivalence Java/view/Logical_EquivalenceGUI.java
@@ -1,4 +1,27 @@
 package view;
-public class Logical_EquivalenceGUI {
-    
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.HBox;
+import javafx.stage.Stage;
+
+public class Logical_EquivalenceGUI extends Application{
+    @Override
+    public void start(Stage stage) throws Exception {
+        HBox hBox = new HBox(); 
+
+        stage.setScene(new Scene(new Label()));
+        stage.setTitle("Template");
+        stage.show();
+    }
+    private Button makeButton(String innerText) {
+        Button button = new Button(innerText);
+
+        return button;
+    }
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
