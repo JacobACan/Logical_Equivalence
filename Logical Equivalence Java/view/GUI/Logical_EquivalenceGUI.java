aae890c27d255714a86bfca0e3fcb8c747d9e932
Jacob Canedy <jcanedy19@gmail.com>
2022-04-19 18:57:37 -0400

    PC -> IntelliJ

--- Logical Equivalence Java/view/GUI/Logical_EquivalenceGUI.java
+++ Logical Equivalence Java/view/GUI/Logical_EquivalenceGUI.java
@@ -1,19 +1,20 @@
-package view;
+package view.GUI;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
 
 public class Logical_EquivalenceGUI extends Application{
     @Override
     public void start(Stage stage) throws Exception {
-        HBox hBox = new HBox(); 
-
-        stage.setScene(new Scene(new Label()));
-        stage.setTitle("Template");
+        Parent root = FXMLLoader.load(getClass().getResource("Logical_Equivalence.fxml"));
+        Scene scene = new Scene(root, 300, 275);
+        scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+        stage.setTitle("Logical Equivalence Calculator");
+        stage.setScene(scene);
         stage.show();
     }
     private Button makeButton(String innerText) {
