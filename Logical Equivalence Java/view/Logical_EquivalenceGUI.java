67e5c31605d3fdcbce29c8eda7dd7f069ddd3500
Jacob Canedy <jcanedy19@gmail.com>
2022-04-20 03:23:55 -0400

    Project: removed unused variables

--- Logical Equivalence Java/view/Logical_EquivalenceGUI.java
+++ Logical Equivalence Java/view/Logical_EquivalenceGUI.java
@@ -3,7 +3,6 @@ package view;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.layout.BackgroundImage;
 import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
 
